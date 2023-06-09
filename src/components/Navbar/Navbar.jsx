24257153c752b1ff4bb78745907b7c2985f16a68
Daniel Quijano <hector.daniel.quijano@gmail.com>
2023-06-09 14:50:58 -0300

    h

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -87,9 +87,19 @@ function Navbar() {
                     <div className="navbar__img-text-conainer">
 
                         <div className="textIntro">
-                            <div className="textIntro__title textoGrisOscuro"><Balancer className="fontOswald fontWeight500 letterSpacing"> Libera Todo Tu Potencial</Balancer></div>
-                            <p className="textIntro__p textoGrisMedio"> <Balancer> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum commodi consectetur cum blanditiis cupiditate, nemo consequatur qui numquam iste ea culpa quae.</Balancer></p>
-                            <a href="https://www.google.com/maps/reserve/v/default?m=ZuYZ98OLOGI&source=pa&gei=C0NiZKqrL7Cj1sQPkLuKyAc&sourceurl=https://www.google.com/search?q%3Dbeauty%2Bport%2Bpeluqueria%26oq%3Dbeauty%2Bport%2Bpeluqueria%26aqs%3Dchrome..69i57.5819j0j1%26sourceid%3Dchrome%26ie%3DUTF-8&hl=es-419" target="blank"><div className="button textoRosaClaro fondoRosaOscuro fontMontserrat">Reserva Tu Turno</div></a>
+                            {windowsSize > 700 ?
+                                <>
+                                    <div className="textIntro__title textoGrisOscuro"><Balancer className="fontOswald fontWeight500 letterSpacing"> Libera Todo Tu Potencial</Balancer></div>
+                                    <p className="textIntro__p textoGrisMedio"> <Balancer> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum commodi consectetur cum blanditiis cupiditate, nemo consequatur qui numquam iste ea culpa quae.</Balancer></p>
+                                    <a href="https://www.google.com/maps/reserve/v/default?m=ZuYZ98OLOGI&source=pa&gei=C0NiZKqrL7Cj1sQPkLuKyAc&sourceurl=https://www.google.com/search?q%3Dbeauty%2Bport%2Bpeluqueria%26oq%3Dbeauty%2Bport%2Bpeluqueria%26aqs%3Dchrome..69i57.5819j0j1%26sourceid%3Dchrome%26ie%3DUTF-8&hl=es-419" target="blank"><div className="button textoRosaClaro fondoRosaOscuro fontMontserrat">Reserva Tu Turno</div></a>
+                                </>
+                                :
+                                <>
+                                    <div className="textIntro__title textoRosaClaro"><Balancer className="fontOswald fontWeight500 letterSpacing"> Libera Todo Tu Potencial</Balancer></div>
+                                    <p className="textIntro__p textoRosaClaro"> <Balancer> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum commodi consectetur cum blanditiis cupiditate, nemo consequatur qui numquam iste ea culpa quae.</Balancer></p>
+                                    <a href="https://www.google.com/maps/reserve/v/default?m=ZuYZ98OLOGI&source=pa&gei=C0NiZKqrL7Cj1sQPkLuKyAc&sourceurl=https://www.google.com/search?q%3Dbeauty%2Bport%2Bpeluqueria%26oq%3Dbeauty%2Bport%2Bpeluqueria%26aqs%3Dchrome..69i57.5819j0j1%26sourceid%3Dchrome%26ie%3DUTF-8&hl=es-419" target="blank"><div className="button textoRosaClaro fondoRosaOscuro fontMontserrat">Reserva Tu Turno</div></a>
+                                </>
+                            }
                         </div>
 
                     </div>
