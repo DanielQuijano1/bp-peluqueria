8a6e27c52183b916c8ca4c0559e340188cf6a46a
Daniel Quijano <hector.daniel.quijano@gmail.com>
2023-06-09 15:59:56 -0300

    GSAJ

--- src/components/Main/flexServicios/Servicios.jsx
+++ src/components/Main/flexServicios/Servicios.jsx
@@ -14,12 +14,12 @@ const responsive = {
     desktop: {
         breakpoint: { max: 3000, min: 1024 },
         items: 3,
-        slidesToSlide: 3 // optional, default to 1.
+        slidesToSlide: 1 // optional, default to 1.
     },
     tablet: {
         breakpoint: { max: 1024, min: 464 },
         items: 2,
-        slidesToSlide: 2 // optional, default to 1.
+        slidesToSlide: 1 // optional, default to 1.
     },
     mobile: {
         breakpoint: { max: 464, min: 0 },
@@ -34,10 +34,14 @@ function Servicios() {
             <section className="main__flex--services espaciadoVH" >
                 <h1 className="textIntro__title fontMontserrat textoCentrado textoGrisOscuro letterSpacing" id="Servicios"><Balancer className="fontOswald fontWeight400"> Nuestros Servicios</Balancer></h1>
 
-                <Carousel className='flexCarrusel' responsive={responsive} infinite={true} autoPlay={true} autoPlaySpeed={3000}>
+                <Carousel className='flexCarrusel' responsive={responsive} infinite={true} autoPlay={true} autoPlaySpeed={5000}>
                     <Card title="Peinados" className="cardImage letterSpacing card1" />
                     <Card title="Spa de Pies" className="cardImage letterSpacing card2" />
                     <Card title="Manicura" className="cardImage letterSpacing card3" />
+                    <Card title="Spa de Pies" className="cardImage letterSpacing card2" />
+                    <Card title="Spa de Pies" className="cardImage letterSpacing card2" />
+                    <Card title="Spa de Pies" className="cardImage letterSpacing card2" />
+                    <Card title="Spa de Pies" className="cardImage letterSpacing card2" />
                 </Carousel>;
 
             </section>
