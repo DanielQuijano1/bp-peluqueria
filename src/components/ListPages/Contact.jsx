c0e19902f5bcb94cd5adf810f5f5a9ff91d66184
Daniel <hector.daniel.quijano@gmail.com>
2023-09-14 21:58:27 -0300

    change color title

--- src/components/ListPages/Contact.jsx
+++ src/components/ListPages/Contact.jsx
@@ -5,18 +5,12 @@ function Contact({ windowsSize }) {
     return (
         <>
             <div className="width100vw widthTitle imgFondoContact centrarGrid ">
-                {windowsSize > 700 ?
                 <h1 className="textoGrisOscuro textoGrisClaro paddingTitle textoCentrado backgroundTitle bordersTop">Cómo Encontrarnos</h1>
-                :
-                <h1 className="textoGrisOscuro textoGrisClaro paddingTitle textoCentrado backgroundTitle bordersTop ">Cómo Encontrarnos</h1>
-                
-                }
-                
             </div>
             <div className="width100vw centrarGridCentro">
                 <div className="displayFlex flexDirectionColumn spaceAround gap1em">
                     <MapaGoogle windowsSize={windowsSize} />
-                    <div className="noPaddingTop"/>
+                    <div className="noPaddingTop" />
                     <div className="textoGrisOscuro textoCentrado" >
                         <section className="padding1em ">
                             <h3>Puerto Madero</h3>
@@ -25,8 +19,8 @@ function Contact({ windowsSize }) {
                             <p>Lunes a Sábados 10hs a 20hs</p>
                         </section>
                     </div>
-                    <div className="noPaddingTop"/>
-                    <div className="padding1em"/>   
+                    <div className="noPaddingTop" />
+                    <div className="padding1em" />
                 </div>
             </div>
         </>
--- src/components/ListPages/Services.jsx
+++ src/components/ListPages/Services.jsx
@@ -1,14 +1,55 @@
 import "./../../App.css"
+import Marcas from "./../Main/flexServicios/Marcas";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+
+const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3,
+        slidesToSlide: 1 // optional, default to 1.
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2,
+        slidesToSlide: 1 // optional, default to 1.
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 2,
+        slidesToSlide: 1 // optional, default to 1.
+    }
+};
+
 
 function Services() {
     return (
         <>
             <div className="width100vw widthTitle imgFondoContact centrarGrid">
-                <h1 className="textoGrisOscuro textoGrisClaro paddingTitle textoCentrado backgroundTitle bordersTop">Nuestros Servicios</h1>
+                <h1 className="textoGrisClaro paddingTitle textoCentrado backgroundTitle bordersTop">Nuestros Servicios</h1>
             </div>
-            <div className="width100vw heigth100vh centrarGridCentro">
-                <h1 className="textoGrisOscuro">PRÓXIMAMENTE</h1>
-            </div>
+            <section className="textoCentrado textoGrisOscuro">
+                <h1>Peluquería</h1>
+                <Carousel className="width80vw flexServicios" responsive={responsive} infinite={true} autoPlay={true} autoPlaySpeed={2000} removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}>
+                    <Marcas className=" cardServicio " />
+                    <Marcas className=" cardServicio " />
+                    <Marcas className=" cardServicio " />
+                    <Marcas className=" cardServicio " />
+                    <Marcas className=" cardServicio " />
+                </Carousel>
+            </section>
+            <section className="textoCentrado textoGrisOscuro">
+                <h1>Spa de Pies</h1>
+            </section>
+            <section className="textoCentrado textoGrisOscuro">
+                <h1>Spa de Manos</h1>
+            </section>
         </>
     )
 }
